a36b19ef5035d062e2e71d263d11283eefa49bfc
LucaO05 <LucaOestreich2804@gmail.com>
2025-04-04 12:03:00 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
 from gpio_service import GPIOService
 from email_service import EmailService
 from config import Config
-
-def main():
-    email_service = EmailService(Config.smtp_server, Config.smtp_port, Config.sender_Email, Config.sender_Password, Cofrom gpio_service import GPIOService
-from email_service import EmailService
-from config import Config
 import time
 
 def main():
@@ -39,25 +34,6 @@ def main():
         print("Cleaning up resources...")
         gpio_service.cleanup()
         print("Program ended.")
-        
-if __name__ == "__main__":
-    main()nfig.recipient_Email)
-    gpio_service = GPIOService(Config.sensorPin, Config.lampPin, Config.switch)
-    
-    # Set up email_service as an attribute of gpio_service
-    gpio_service.email_service = email_service
-    
-    try: 
-        gpio_service.setup()
-        gpio_service.add_event_detect()
-        while True:
-            pass
-    except KeyboardInterrupt:
-        print("\nBeende Programm...")
-    except Exception as e:
-        print(f"Fehler: {e}")
-    finally:
-        gpio_service.cleanup()
         
 if __name__ == "__main__":
     main()
